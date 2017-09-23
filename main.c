9522ce4fc831b87871d5a24d896d506030b26128
takumin <takumiiinn@gmail.com>
2017-09-24 00:09:02 +0900

    fix

--- main.c
+++ main.c
@@ -1,14 +1,27 @@
 extern const int i;
 extern int j;
 extern int m;
+extern int dtcm;
 extern void main(void);
+extern void itcm(void);
 
 const int i = 10;
 int j = 10;
 int m;
+
 void main(void) {
+  itcm();
   for (int k = 0; k < i; k++) {
     j = i + k;
     m = j;
+  }
+}
+
+__attribute__((section(".dtcm"))) int dtcm;
+
+__attribute__((section(".itcm"))) void itcm(void) {
+  for (int k = 0; k < i; k++) {
+    j = i + k;
+    dtcm = j;
   }
 }
